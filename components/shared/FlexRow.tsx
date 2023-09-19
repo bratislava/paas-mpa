9d0e11b911a7703869dd64a010a3c8bbc5f5c7dc
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2023-09-19 13:36:54 +0200

    init Purchase screen

---
+++ components/shared/FlexRow.tsx
@@ -0,0 +1,14 @@
+import { clsx } from 'clsx'
+import React, { ReactNode } from 'react'
+import { View } from 'react-native'
+
+type Props = {
+  children: ReactNode
+  cn?: string
+}
+
+const FlexRow = ({ children, cn }: Props) => {
+  return <View className={clsx('flex-row justify-between', cn)}>{children}</View>
+}
+
+export default FlexRow
