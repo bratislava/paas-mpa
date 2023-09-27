2fa0f3c6b3a828b1ae7d223ffe7a1adb5697d940
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2023-09-27 09:34:12 +0200

    [#74] implement DateTime picker

--- app/purchase/custom-time.tsx
+++ app/purchase/custom-time.tsx
@@ -1,22 +1,29 @@
+import { router, useGlobalSearchParams } from 'expo-router'
 import React from 'react'
 
-import TextInput from '@/components/inputs/TextInput'
+import { PurchaseSearchParams } from '@/app/purchase/index'
+import DateTimePicker from '@/components/controls/date-time/DateTimePicker'
 import Field from '@/components/shared/Field'
 import ScreenContent from '@/components/shared/ScreenContent'
 import ScreenView from '@/components/shared/ScreenView'
-import Typography from '@/components/shared/Typography'
 import { useTranslation } from '@/hooks/useTranslation'
 
 const Page = () => {
   const t = useTranslation('PurchaseScreen')
+  const searchParams = useGlobalSearchParams<PurchaseSearchParams>()
 
   return (
     <ScreenView title={t('customParkingTimeTitle')}>
       <ScreenContent>
-        <Typography>TODO set custom time after confirm</Typography>
-        <Typography>TODO datetime picker</Typography>
-        <Field label="Temporary field: custom time in minutes">
-          <TextInput keyboardType="numeric" />
+        <Field label={t('customParkingTimeFieldLabel')}>
+          <DateTimePicker
+            onConfirm={(date) => {
+              router.push({
+                pathname: '/purchase/',
+                params: { ...searchParams, customParkingTime: date.toISOString() },
+              })
+            }}
+          />
         </Field>
       </ScreenContent>
     </ScreenView>
