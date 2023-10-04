ebf9deb3b1a4ddcc1c9aee3d569061a84e6fe314
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2023-10-04 16:29:31 +0200

    init tab view lib

--- app/tickets/index.tsx
+++ app/tickets/index.tsx
@@ -1,19 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useWindowDimensions } from 'react-native'
+import { SceneMap, TabView } from 'react-native-tab-view'
 
+import TabBar from '@/components/navigation/TabBar'
 import ScreenContent from '@/components/shared/ScreenContent'
 import ScreenView from '@/components/shared/ScreenView'
 import Typography from '@/components/shared/Typography'
 import { useTranslation } from '@/hooks/useTranslation'
 
+const ActiveTicketsRoute = () => (
+  <ScreenContent>
+    <Typography>Active</Typography>
+  </ScreenContent>
+)
+
+const HistoryRoute = () => (
+  <ScreenContent>
+    <Typography>History</Typography>
+  </ScreenContent>
+)
+
+const renderScene = SceneMap({
+  active: ActiveTicketsRoute,
+  history: HistoryRoute,
+})
+
 // TODO
 const Page = () => {
   const t = useTranslation('Tickets')
+  const layout = useWindowDimensions()
+
+  const [index, setIndex] = useState(0)
+  const [routes] = useState([
+    { key: 'active', title: 'Active tickets' },
+    { key: 'history', title: 'History' },
+  ])
 
   return (
     <ScreenView title={t('title')}>
-      <ScreenContent>
-        <Typography variant="h1">TODO</Typography>
-      </ScreenContent>
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={{ width: layout.width }}
+        renderTabBar={(props) => <TabBar {...props} />}
+      />
     </ScreenView>
   )
 }
