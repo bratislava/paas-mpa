afec5a2c47021fef3446c808e237db24b6dd7f4c
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2023-10-19 20:36:25 +0200

    send request to verify email

--- app/parking-cards/enter-paas-account.tsx
+++ app/parking-cards/enter-paas-account.tsx
@@ -1,16 +1,22 @@
+import { useMutation } from '@tanstack/react-query'
+import { router } from 'expo-router'
 import { useState } from 'react'
 
 import TextInput from '@/components/inputs/TextInput'
+import Button from '@/components/shared/Button'
 import Field from '@/components/shared/Field'
 import Panel from '@/components/shared/Panel'
 import ScreenContent from '@/components/shared/ScreenContent'
 import ScreenView from '@/components/shared/ScreenView'
 import Typography from '@/components/shared/Typography'
 import { useTranslation } from '@/hooks/useTranslation'
+import { clientApi } from '@/modules/backend/client-api'
+import { VerifyEmailsDto } from '@/modules/backend/openapi-generated'
 
 /*
  * TODO
  * - [ ] add email validation
+ * - [ ] handle error on mutation (email not sent)
  */
 
 const Page = () => {
@@ -21,14 +27,33 @@ const Page = () => {
   // TODO add email validation
   const isValid = email.length > 0
 
+  const mutation = useMutation({
+    mutationFn: (bodyInner: VerifyEmailsDto) =>
+      clientApi.verifiedEmailsControllerSendEmailVerificationEmails(bodyInner),
+    onError: (error) => {
+      // TODO handle error, show snackbar?
+      // Handled in mutation to be sure that snackbar is shown on error
+      console.log('error', error)
+    },
+  })
+
+  const handlePress = () => {
+    const body: VerifyEmailsDto = {
+      emails: [email],
+    }
+
+    mutation.mutate(body, {
+      onSuccess: () =>
+        router.push({
+          pathname: '/parking-cards/verification-sent',
+          params: { emailToVerify: email },
+        }),
+    })
+  }
+
   return (
     <ScreenView title={t('addCardsTitle')}>
-      <ScreenContent
-        continueProps={{
-          href: `/parking-cards/verification-sent?emailToVerify=${email}`,
-          isDisabled: !isValid,
-        }}
-      >
+      <ScreenContent>
         <Field label={t('emailField')}>
           <TextInput
             keyboardType="email-address"
@@ -41,6 +66,10 @@ const Page = () => {
         <Panel>
           <Typography>{t('instructions')}</Typography>
         </Panel>
+
+        <Button onPress={handlePress} disabled={!isValid} loading={mutation.isPending}>
+          Continue
+        </Button>
       </ScreenContent>
     </ScreenView>
   )
--- app/parking-cards/index.tsx
+++ app/parking-cards/index.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from '@tanstack/react-query'
 import { Link, useNavigation } from 'expo-router'
 import React, { useEffect, useState } from 'react'
 import { ScrollView, useWindowDimensions, View } from 'react-native'
@@ -14,8 +15,18 @@ import ScreenContent from '@/components/shared/ScreenContent'
 import ScreenView from '@/components/shared/ScreenView'
 import Typography from '@/components/shared/Typography'
 import { useTranslation } from '@/hooks/useTranslation'
+import { clientApi } from '@/modules/backend/client-api'
 
 const Active = () => {
+  const { data: response } = useQuery({
+    queryKey: ['ParkingCardsActive'],
+    queryFn: () => clientApi.parkingCardsControllerGetParkingCards(),
+  })
+
+  const cards = response?.data.parkingCards ?? []
+
+  console.log('PARKING CARDS', cards)
+
   return (
     <ScrollView>
       <ScreenContent>
