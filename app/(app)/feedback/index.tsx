3d637e7cd2ca594bf62111275a5fc49110222308
Lukas Bilicky <lukybil@gmail.com>
2023-12-25 14:29:41 +0100

    added api call, success/error handling, small screen responsive

--- app/(app)/feedback/index.tsx
+++ app/(app)/feedback/index.tsx
@@ -1,9 +1,11 @@
+import { useMutation } from '@tanstack/react-query'
 import { router } from 'expo-router'
-import { Dispatch, SetStateAction, useCallback, useRef, useState } from 'react'
+import { Dispatch, SetStateAction, useCallback, useEffect, useRef, useState } from 'react'
 import {
   Keyboard,
   NativeSyntheticEvent,
   Pressable,
+  ScrollView,
   TextInput as NativeTextInput,
   TextInputChangeEventData,
 } from 'react-native'
@@ -11,22 +13,34 @@ import {
 import TextInput from '@/components/inputs/TextInput'
 import ScreenContent from '@/components/screen-layout/ScreenContent'
 import ScreenView from '@/components/screen-layout/ScreenView'
+import { useSnackbar } from '@/components/screen-layout/Snackbar/useSnackbar'
 import Button from '@/components/shared/Button'
 import Chip from '@/components/shared/Chip'
 import Field from '@/components/shared/Field'
 import FlexRow from '@/components/shared/FlexRow'
 import PressableStyled from '@/components/shared/PressableStyled'
 import { useTranslation } from '@/hooks/useTranslation'
+import { clientApi } from '@/modules/backend/client-api'
+import { FeedbackDto, FeedbackType } from '@/modules/backend/openapi-generated'
 
 const FeedbackScreen = () => {
   const t = useTranslation('FeedbackScreen')
+  const snackbar = useSnackbar()
+
   const emailRef = useRef<NativeTextInput>(null)
   const messageRef = useRef<NativeTextInput>(null)
+
   const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
   const [shouldVerifyEmail, setShouldVerifyEmail] = useState(false)
   const [shouldVerifyMessage, setShouldVerifyMessage] = useState(false)
   const [feedbackType, setFeedbackType] = useState<'bug' | 'proposal'>('bug')
+
+  const mutation = useMutation({
+    mutationFn: (feedbackDto: FeedbackDto) => {
+      return clientApi.systemControllerSendFeedback(feedbackDto)
+    },
+  })
 
   const handleTypePress = useCallback(
     (type: 'bug' | 'proposal') => () => {
@@ -61,60 +75,85 @@ const FeedbackScreen = () => {
   }, [])
 
   const handleSubmit = useCallback(() => {
-    router.replace('/feedback/success')
-  }, [])
+    // eslint-disable-next-line no-underscore-dangle
+    const stringType: FeedbackType = feedbackType === 'bug' ? FeedbackType._0 : FeedbackType._1
+    // TODO: bug fix, FeedbackType should be a number
+    const type = Number.parseInt(stringType, 10)
+    mutation.mutate({ email, message, type: type as unknown as FeedbackType })
+  }, [email, feedbackType, message, mutation])
 
-  const isValidEmail = !shouldVerifyEmail || !!email || email.includes('@')
+  useEffect(() => {
+    if (mutation.isSuccess) {
+      router.replace('/feedback/success')
+      mutation.reset()
+    } else if (mutation.isError) {
+      mutation.reset()
+    }
+  }, [mutation, t, snackbar])
+
+  const isValidEmail = !shouldVerifyEmail || (!!email && email.includes('@'))
   const isValidMessage = !shouldVerifyMessage || message.length > 0
+  const isDisabled = !isValidEmail || !isValidMessage || !email || !message
 
   return (
     <ScreenView title={t('title')}>
       <Pressable onPress={Keyboard.dismiss} className="flex-1">
-        <ScreenContent className="flex-1">
-          <Field
-            label={t('emailAddress')}
-            errorMessage={isValidEmail ? undefined : t('emailAddressInvalid')}
-          >
-            <TextInput
-              ref={emailRef}
-              value={email}
-              onChange={handleInputChange(setEmail)}
-              onFocus={handleEmailFocus}
-              onBlur={handleEmailBlur}
-              onSubmitEditing={handleEmailSubmit}
-            />
-          </Field>
-          <Field label={t('type')}>
-            <FlexRow>
-              {(['bug', 'proposal'] as const).map((label) => (
-                <PressableStyled onPress={handleTypePress(label)} className="h-[48px] flex-1">
-                  <Chip label={t(label)} key={label} isActive={feedbackType === label} />
-                </PressableStyled>
-              ))}
-            </FlexRow>
-          </Field>
-          <Field
-            style={{ flex: 1 }}
-            label={t('yourMessage')}
-            errorMessage={isValidMessage ? undefined : t('yourMessageInvalid')}
-          >
-            <TextInput
-              ref={messageRef}
-              value={message}
-              multiline
-              className="h-auto grow"
-              textAlignVertical="top"
-              blurOnSubmit={false}
-              numberOfLines={30}
-              onChange={handleInputChange(setMessage)}
-              onFocus={handleMessageFocus}
-              onBlur={handleMessageBlur}
-            />
-          </Field>
-          <Button variant="primary" onPress={handleSubmit}>
-            {t('send')}
-          </Button>
-        </ScreenContent>
+        <ScrollView>
+          <ScreenContent className="flex-1">
+            <Field
+              label={t('emailAddress')}
+              errorMessage={isValidEmail ? undefined : t('emailAddressInvalid')}
+            >
+              <TextInput
+                ref={emailRef}
+                value={email}
+                onChange={handleInputChange(setEmail)}
+                onFocus={handleEmailFocus}
+                onBlur={handleEmailBlur}
+                onSubmitEditing={handleEmailSubmit}
+              />
+            </Field>
+            <Field label={t('type')}>
+              <FlexRow>
+                {(['bug', 'proposal'] as const).map((label) => (
+                  <PressableStyled
+                    key={label}
+                    onPress={handleTypePress(label)}
+                    className="h-[48px] flex-1"
+                  >
+                    <Chip label={t(label)} key={label} isActive={feedbackType === label} />
+                  </PressableStyled>
+                ))}
+              </FlexRow>
+            </Field>
+            <Field
+              style={{ flex: 1 }}
+              label={t('yourMessage')}
+              errorMessage={isValidMessage ? undefined : t('yourMessageInvalid')}
+            >
+              <TextInput
+                ref={messageRef}
+                value={message}
+                multiline
+                className="h-auto grow"
+                textAlignVertical="top"
+                blurOnSubmit={false}
+                numberOfLines={15}
+                onChange={handleInputChange(setMessage)}
+                onFocus={handleMessageFocus}
+                onBlur={handleMessageBlur}
+              />
+            </Field>
+            <Button
+              variant="primary"
+              onPress={handleSubmit}
+              loading={mutation.isPending}
+              disabled={isDisabled}
+            >
+              {t('send')}
+            </Button>
+          </ScreenContent>
+        </ScrollView>
       </Pressable>
     </ScreenView>
   )
