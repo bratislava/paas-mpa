aec8eb3397fe152bdfaaa9b9cc2d2faa131174fe
PavelFilo <pvlfilo@gmail.com>
2024-03-11 10:57:06 +0100

    fix: tailwind-merge extend function

--- utils/clsx.ts
+++ utils/clsx.ts
@@ -1,4 +1,12 @@
 import cn from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { extendTailwindMerge } from 'tailwind-merge'
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      'font-size': ['text-h1', 'text-h2', 'text-h3', 'text-base', 'text-sm'],
+    },
+  },
+})
 
 export const clsx = (...args: cn.ClassValue[]) => twMerge(cn(...args))
