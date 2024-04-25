d4ac9f95b2b9e5dea1aee3a7c2b06fd8abdf77ac
PavelFilo <61841194+PavelFilo@users.noreply.github.com>
2024-04-25 11:19:22 +0200

    improvement: texts about added LicencePlates (#476)
    * improvement: texts about added LicencePlates
    
    * imp: better condition
    
    * line break
    
    * refactor: translation changes based on comments + class color

--- i18n.config.js
+++ i18n.config.js
@@ -14,20 +14,12 @@ const resources = {
   },
 }
 
-/**
- *  Migration guides suggest to polyfill Intl using intl-pluralrules.
- *  https://www.i18next.com/misc/migration-guide#v20.x.x-to-v21.0.0
- *
- *  It's installed and imported in main layout, but it seems not to work, as mentioned here: https://github.com/i18next/react-i18next/issues/1495#issuecomment-1113990587
- *  So we use v3 for now. If we get the polyfill to work, compatibilityJSON mode should be removed.
- */
 i18n
   .use(initReactI18next)
   .use(languageDetectorPlugin)
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     resources,
-    compatibilityJSON: 'v3',
     fallbackLng: 'en',
     debug: environment.deployment === 'development',
   })
