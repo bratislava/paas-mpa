852c31c3f9e0db89ff1b45a777664b1b2a22dcd1
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-05-28 10:37:49 +0200

    #507 Add missing `modules` director into i18next parser

--- i18next-parser.config.js
+++ i18next-parser.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   locales: ['sk', 'en'],
-  input: '(app|components|hooks|state|utils)/**/*.{tsx,ts}',
+  input: '(app|components|hooks|state|utils|modules)/**/*.{tsx,ts}',
   output: './translations/$LOCALE.json',
   // if set to true preserves old values in a separate json file
   createOldCatalogs: false,
