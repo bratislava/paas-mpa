196dd478a540e6b71ef32ef93a7b6de2fb144839
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-06-04 09:51:16 +0200

    Remove unnecessary hook and improve translations

--- components/purchase/PaymentStatus/ErrorContent.tsx
+++ components/purchase/PaymentStatus/ErrorContent.tsx
@@ -1,7 +1,7 @@
-import { useTicketPurchaseTranslation } from '@/components/purchase/PaymentStatus/useTicketPurchaseTranslation'
 import ContentWithAvatar from '@/components/screen-layout/ContentWithAvatar'
 import Panel from '@/components/shared/Panel'
 import Typography from '@/components/shared/Typography'
+import { useTranslation } from '@/hooks/useTranslation'
 
 type Props = {
   message: string
@@ -13,13 +13,24 @@ const ErrorContent = ({
   // We cannot determine the purchaseType if useQuery has errored, because there is no `data`, so we display default payment error
   purchaseType = 'payment',
 }: Props) => {
-  const translationMap = useTicketPurchaseTranslation()
+  const { t } = useTranslation()
+
+  const translationMap = {
+    payment: {
+      title: t('PurchaseScreen.paymentStatus.failed.payment.title'),
+      text: t('PurchaseScreen.paymentStatus.failed.payment.text'),
+    },
+    prolongation: {
+      title: t('PurchaseScreen.paymentStatus.failed.prolongation.title'),
+      text: t('PurchaseScreen.paymentStatus.failed.prolongation.text'),
+    },
+  }
 
   return (
     <ContentWithAvatar
       variant="error"
-      title={translationMap[purchaseType].failedTitle}
-      text={translationMap[purchaseType].failedText}
+      title={translationMap[purchaseType].title}
+      text={translationMap[purchaseType].text}
     >
       <Panel className="bg-negative-light">
         <Typography>{message}</Typography>
--- components/purchase/PaymentStatus/SuccessContent.tsx
+++ components/purchase/PaymentStatus/SuccessContent.tsx
@@ -1,6 +1,6 @@
-import { useTicketPurchaseTranslation } from '@/components/purchase/PaymentStatus/useTicketPurchaseTranslation'
 import ContentWithAvatar from '@/components/screen-layout/ContentWithAvatar'
 import BoughtTicket from '@/components/tickets/BoughtTicket'
+import { useTranslation } from '@/hooks/useTranslation'
 import { TicketDto } from '@/modules/backend/openapi-generated'
 
 type Props = {
@@ -9,13 +9,24 @@ type Props = {
 }
 
 const SuccessContent = ({ ticket, purchaseType }: Props) => {
-  const translationMap = useTicketPurchaseTranslation()
+  const { t } = useTranslation()
+
+  const translationMap = {
+    payment: {
+      title: t('PurchaseScreen.paymentStatus.successful.payment.title'),
+      text: t('PurchaseScreen.paymentStatus.successful.payment.text'),
+    },
+    prolongation: {
+      title: t('PurchaseScreen.paymentStatus.successful.prolongation.title'),
+      text: t('PurchaseScreen.paymentStatus.successful.prolongation.text'),
+    },
+  }
 
   return (
     <ContentWithAvatar
       variant="success"
-      title={translationMap[purchaseType].successTitle}
-      text={translationMap[purchaseType].successText}
+      title={translationMap[purchaseType].title}
+      text={translationMap[purchaseType].text}
     >
       <BoughtTicket ticket={ticket} />
     </ContentWithAvatar>
