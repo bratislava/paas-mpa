f27b857cc981ce4f00c1e3bff27406de465131b4
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2024-07-23 09:40:43 +0200

    #570 Add udr id and name to FreeTicket message (#599)
    * #570 Add udr id and name to FreeTicket message
    
    * #570 Use Markdown and bold style on udr an ecv in error message

--- components/purchase/PurchaseErrorPanel.tsx
+++ components/purchase/PurchaseErrorPanel.tsx
@@ -2,12 +2,13 @@ import { UseQueryResult } from '@tanstack/react-query'
 import { isAxiosError } from 'axios'
 import React from 'react'
 
+import Markdown from '@/components/shared/Markdown'
 import Panel from '@/components/shared/Panel'
-import Typography from '@/components/shared/Typography'
 import { useTranslation } from '@/hooks/useTranslation'
 import { GetTicketPriceResponseDto } from '@/modules/backend/openapi-generated'
 import { usePurchaseStoreContext } from '@/state/PurchaseStoreProvider/usePurchaseStoreContext'
 import { isPricingApiError } from '@/utils/errorPricingApi'
+import { isDefined } from '@/utils/isDefined'
 
 type Props = {
   priceQuery: UseQueryResult<GetTicketPriceResponseDto, Error>
@@ -48,13 +49,13 @@ const PurchaseErrorPanel = ({ priceQuery }: Props) => {
     priceQuery.error.response?.status === 422 &&
     isPricingApiError(priceQuery.error.response.data) ? (
     <Panel className="bg-negative-light px-5 py-4">
-      <Typography>
+      <Markdown>
         {/* TODO translation */}
         {t(`PurchaseScreen.Errors.${priceQuery.error.response.data.status}`, {
           ecv: vehicle?.vehiclePlateNumber,
-          udr: udr?.name,
+          udr: [udr?.udrId, udr?.name].filter(isDefined).join(' '),
         })}
-      </Typography>
+      </Markdown>
     </Panel>
   ) : null
 }
