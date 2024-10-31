c50336fa7ee3570e091facd10170a4b534cd630b
PavelFilo <pvlfilo@gmail.com>
2024-10-31 13:08:14 +0100

    #680 add comment

--- components/controls/payment-methods/types.ts
+++ components/controls/payment-methods/types.ts
@@ -2,6 +2,7 @@ import { StoredPaymentMethodDto } from '@/modules/backend/openapi-generated'
 
 type PaymentMethodType = 'payment-card' | 'apple-pay' | 'google-pay' | 'card'
 
+// ID should come from API... so this won't work until API is fixed
 export type PaymentMethod =
   | {
       type: Exclude<PaymentMethodType, 'card'>
