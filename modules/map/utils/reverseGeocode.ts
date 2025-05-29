0d207a8db741b943fac5652104949181a042bfb1
PavelFilo <pvlfilo@gmail.com>
2025-05-29 12:31:34 +0200

    remove redundant const for reverse geocode

--- modules/map/utils/reverseGeocode.ts
+++ modules/map/utils/reverseGeocode.ts
@@ -5,15 +5,15 @@ import { environment } from '@/environment'
 import { GeocodingFeature } from '@/modules/arcgis/types'
 import { CITY_BOUNDS } from '@/modules/map/constants'
 
+const RESULTS_LIMIT = 1
+
 export const reverseGeocode = async (position: Position) => {
   const [lon, lat] = position
-  const accessToken = environment.mapboxPublicKey
-  const limit = 1
 
   const bbox = [...CITY_BOUNDS.sw, ...CITY_BOUNDS.ne].join(',')
 
   const result = await axios.get(
-    `https://api.mapbox.com/geocoding/v5/mapbox.places/${lon},${lat}.json?limit=${limit}&bbox=${bbox}&limit=1&country=sk&access_token=${accessToken}`,
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/${lon},${lat}.json?limit=${RESULTS_LIMIT}&bbox=${bbox}&limit=1&country=sk&access_token=${environment.mapboxPublicKey}`,
   )
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
