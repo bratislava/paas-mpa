1e17f4b6b20a5ae228ec694b39164042993a1bb7
PavelFilo <61841194+PavelFilo@users.noreply.github.com>
2025-06-25 09:12:25 +0200

    #748 Add future cards and filters (#760)
    * #748 add future cards and filters
    
    * #748 change padding
    
    * #748 add validFrom to to cards
    
    * #748 changes based on comments
    
    * #748 change omit to pick in BonusCard

--- components/parking-cards/cards/BonusCard.tsx
+++ components/parking-cards/cards/BonusCard.tsx
@@ -3,16 +3,21 @@ import React from 'react'
 import CardContentItem from '@/components/parking-cards/base/CardContentItem'
 import ParkingCardBase from '@/components/parking-cards/base/ParkingCardBase'
 import ParkingCardContent from '@/components/parking-cards/base/ParkingCardContent'
+import { ParkingCardValidityItems } from '@/components/parking-cards/base/ParkingCardValidityItems'
 import { CommonParkingCardProps } from '@/components/parking-cards/ParkingCard'
 import Divider from '@/components/shared/Divider'
 import Typography from '@/components/shared/Typography'
-import { useLocale, useTranslation } from '@/hooks/useTranslation'
+import { useTranslation } from '@/hooks/useTranslation'
 import { formatBalance } from '@/utils/formatBalance'
-import { formatValidUntilDate } from '@/utils/formatValidUntilDate'
 
 type BonusCardProps = Pick<
   CommonParkingCardProps,
-  'balanceSeconds' | 'originalBalanceSeconds' | 'zoneName' | 'licencePlate' | 'validUntil'
+  | 'zoneName'
+  | 'licencePlate'
+  | 'validUntil'
+  | 'validFrom'
+  | 'originalBalanceSeconds'
+  | 'balanceSeconds'
 >
 
 const BonusCard = ({
@@ -21,16 +26,19 @@ const BonusCard = ({
   zoneName,
   licencePlate,
   validUntil,
+  validFrom,
 }: BonusCardProps) => {
   const { t } = useTranslation()
-  const locale = useLocale()
 
   return (
     <ParkingCardBase variant="bonus">
       <ParkingCardContent>
         <Typography variant="small">{zoneName}</Typography>
+
         <Typography variant="small">{licencePlate}</Typography>
+
         <Divider className="bg-divider" />
+
         <CardContentItem
           description={t('ParkingCards.remainingCredit')}
           value={
@@ -39,12 +47,8 @@ const BonusCard = ({
               : ''
           }
         />
-        {validUntil ? (
-          <CardContentItem
-            description={t('ParkingCards.validUntil')}
-            value={formatValidUntilDate(validUntil, locale)}
-          />
-        ) : null}
+
+        <ParkingCardValidityItems validFrom={validFrom} validUntil={validUntil} />
       </ParkingCardContent>
     </ParkingCardBase>
   )
--- components/parking-cards/cards/ResidentCard.tsx
+++ components/parking-cards/cards/ResidentCard.tsx
@@ -1,35 +1,29 @@
 import React from 'react'
 
-import CardContentItem from '@/components/parking-cards/base/CardContentItem'
 import ParkingCardBase from '@/components/parking-cards/base/ParkingCardBase'
 import ParkingCardContent from '@/components/parking-cards/base/ParkingCardContent'
+import { ParkingCardValidityItems } from '@/components/parking-cards/base/ParkingCardValidityItems'
 import { CommonParkingCardProps } from '@/components/parking-cards/ParkingCard'
 import Divider from '@/components/shared/Divider'
 import Typography from '@/components/shared/Typography'
-import { useLocale, useTranslation } from '@/hooks/useTranslation'
-import { formatValidUntilDate } from '@/utils/formatValidUntilDate'
 
-type ResidentCardProps = Pick<CommonParkingCardProps, 'zoneName' | 'licencePlate' | 'validUntil'>
+type ResidentCardProps = Pick<
+  CommonParkingCardProps,
+  'zoneName' | 'licencePlate' | 'validUntil' | 'validFrom'
+>
 
-const ResidentCard = ({ zoneName, licencePlate, validUntil }: ResidentCardProps) => {
-  const { t } = useTranslation()
-  const locale = useLocale()
+const ResidentCard = ({ zoneName, licencePlate, validUntil, validFrom }: ResidentCardProps) => (
+  <ParkingCardBase variant="resident">
+    <ParkingCardContent>
+      <Typography variant="small">{zoneName}</Typography>
 
-  return (
-    <ParkingCardBase variant="resident">
-      <ParkingCardContent>
-        <Typography variant="small">{zoneName}</Typography>
-        <Typography variant="small">{licencePlate}</Typography>
-        <Divider className="bg-dark" />
-        {validUntil ? (
-          <CardContentItem
-            description={t('ParkingCards.validUntil')}
-            value={formatValidUntilDate(validUntil, locale)}
-          />
-        ) : null}
-      </ParkingCardContent>
-    </ParkingCardBase>
-  )
-}
+      <Typography variant="small">{licencePlate}</Typography>
+
+      <Divider className="bg-dark" />
+
+      <ParkingCardValidityItems validFrom={validFrom} validUntil={validUntil} />
+    </ParkingCardContent>
+  </ParkingCardBase>
+)
 
 export default ResidentCard
