106e415b7bf7f4c4dad411338a005f16ab78b395
PavelFilo <pvlfilo@gmail.com>
2025-10-26 13:25:44 +0100

    Add connect with city account button

---
+++ components/notifications/ConnectWithCityAccountMenuLink.tsx
@@ -0,0 +1,31 @@
+import { useRouter } from 'expo-router'
+import { View } from 'react-native'
+
+import CityAccountImage from '@/assets/images/city-account-image.svg'
+import Icon from '@/components/shared/Icon'
+import PressableStyled from '@/components/shared/PressableStyled'
+import Typography from '@/components/shared/Typography'
+import { useTranslation } from '@/hooks/useTranslation'
+
+export const ConnectWithCityAccountMenuLink = () => {
+  const { t } = useTranslation()
+  const router = useRouter()
+
+  const handlePress = () => {
+    router.push('/notifications/settings')
+  }
+
+  return (
+    <PressableStyled className="w-full overflow-hidden rounded bg-light" onPress={handlePress}>
+      <CityAccountImage width="100%" preserveAspectRatio="xMidYMid slice" />
+
+      <View className="flex-row items-center justify-between p-3 g-2">
+        <Typography variant="default-bold">
+          {t('bloomreachNotifications.button.connectWithCityAccount')}
+        </Typography>
+
+        <Icon name="chevron-right" />
+      </View>
+    </PressableStyled>
+  )
+}
