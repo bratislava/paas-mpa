863adec476f07f1a25483bbffcaf9abfbbe03021
shegood <petrsigut10@gmail.com>
2026-03-30 15:58:57 +0200

    revert the version and remove (OP)

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'PAAS',
     slug: 'paas',
     scheme: 'paasmpa',
-    version: '1.9.1',
+    version: '1.9.0',
     orientation: 'portrait',
     icon: './assets/app/icon.png',
     userInterfaceStyle: 'light',
