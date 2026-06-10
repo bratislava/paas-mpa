ef2dbc377bbe391737ddd88c5306e455b72a10f1
BardunovaBarbora <barbora.bardunova@bratislava.sk>
2026-06-10 09:34:27 +0200

    #624 Update arcgis url to new s3 bucket (#855)
    Update arcgis url to new s3 bucket

--- modules/arcgis/constants.ts
+++ modules/arcgis/constants.ts
@@ -1,5 +1,3 @@
-import { environment } from '@/environment'
-
 export const ARCGIS_URL = 'https://nest-proxy.bratislava.sk/geoportal/hsite/rest/services'
 
-export const STATIC_ARCGIS_URL = `https://${environment.minioBucket}.s3.bratislava.sk/assets/gisdata`
+export const STATIC_ARCGIS_URL = `https://bratislava-static-assets.s3.eu-central-1.amazonaws.com/mpa/gisdata`
