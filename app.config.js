f0c7d5b39be2667dcbab63a258ca525719c14b20
BardunovaBarbora <barbora.bardunova@bratislava.sk>
2026-06-29 10:22:03 +0200

    Update to v 1.11.0

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'PAAS',
     slug: 'paas',
     scheme: 'paasmpa',
-    version: '1.10.0',
+    version: '1.11.0',
     orientation: 'portrait',
     icon: './assets/app/icon.png',
     userInterfaceStyle: 'light',
